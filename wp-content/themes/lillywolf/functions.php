dece18c052d0ee20e18203fbe5acc3b1d426be91
lillywolf <lillywolf@gmail.com>
2014-10-21 02:20:03 -0400

    testing functions.php

--- wp-content/themes/lillywolf/functions.php
+++ wp-content/themes/lillywolf/functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function lillyscript() {
-	wp_enqueue_script('background', get_template_directory_uri() . '/js/background.js', array( 'jquery'));
+	wp_enqueue_script('slider', get_template_directory_uri() . '/js/slider.js', array( 'jquery'));
 }
 add_action( 'wp_enqueue_scripts', 'lillyscript' );
 
