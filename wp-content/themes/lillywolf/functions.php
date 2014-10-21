197597db8381ce8cc09809a9abadcffb3de8dcee
lillywolf <lillywolf@gmail.com>
2014-10-21 01:34:19 -0400

    updated jquery

--- wp-content/themes/lillywolf/functions.php
+++ wp-content/themes/lillywolf/functions.php
@@ -6,12 +6,11 @@ function add_my_script() {
     //     get_template_directory_uri() . '/js/uihelper.js', // this is the location of your script file
     //     array('jquery') // this array lists the scripts upon which your script depends
     // );
-	wp_register_script(
+	wp_enqueue_script(
         'bg',
         get_template_directory_uri() . '/js/background.js',
         array('jquery')
     );
-	wp_enqueue_script('bg');
 }
 
 add_action( 'wp_enqueue_scripts', 'add_my_script' );
