4ac9c5555870be56111f021a0af4203d7dd810b7
lillywolf <lillywolf@gmail.com>
2014-10-21 02:00:05 -0400

    testing functions.php

--- wp-content/themes/lillywolf/functions.php
+++ wp-content/themes/lillywolf/functions.php
@@ -6,14 +6,18 @@ function add_my_script() {
     //     get_template_directory_uri() . '/js/uihelper.js', // this is the location of your script file
     //     array('jquery') // this array lists the scripts upon which your script depends
     // );
-	wp_enqueue_script(
-        'bg',
-        'http://lillywolf-wordpress.herokuapp.com/wp-content/themes/lillywolf/js/background.js',
-        array('jquery')
-    );
+	if ( ! isset( $content_width ) ) {
+		$content_width = 200;
+	}
+	// wp_enqueue_script(
+	//         'bg',
+	//         'http://lillywolf-wordpress.herokuapp.com/wp-content/themes/lillywolf/js/background.js',
+	//         array('jquery')
+	//     );
 	
 }
 
+// wp_enqueue_script( 'jquery' );
 add_action( 'wp_enqueue_scripts', 'add_my_script' );
 
 ?>
