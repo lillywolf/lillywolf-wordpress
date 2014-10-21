6d37307e65ebbcfaa0bfab039a039636f373f161
lillywolf <lillywolf@gmail.com>
2014-10-21 02:23:43 -0400

    testing functions.php

--- wp-content/themes/lillywolf/functions.php
+++ wp-content/themes/lillywolf/functions.php
@@ -1,10 +1,11 @@
 <?php
 
-// function lillyscript() {
+function lillyscript() {
+	wp_enqueue_script( 'twentyfourteen-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20140616', true );
+	
 // 	wp_enqueue_script('slider', get_template_directory_uri() . '/js/slider.js', array( 'jquery'));
-// }
-// add_action( 'wp_enqueue_scripts', 'lillyscript' );
+}
+add_action( 'wp_enqueue_scripts', 'lillyscript' );
 
-wp_enqueue_script( 'twentyfourteen-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20140616', true );
 
 ?>
