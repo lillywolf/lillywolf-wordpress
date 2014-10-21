e0279d2f483a59869c0d939aeff00420ebfc63e8
lillywolf <lillywolf@gmail.com>
2014-10-21 02:50:48 -0400

    testing functions.php

--- wp-content/themes/lillywolf/functions.php
+++ wp-content/themes/lillywolf/functions.php
@@ -1,11 +1,10 @@
 <?php
 
 function lillyscript() {
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('jquery-ui-core');
-	wp_enqueue_script( 'lillybg', get_template_directory_uri() . '/js/background.js', array( 'jquery' ));
-	
-// 	wp_enqueue_script('slider', get_template_directory_uri() . '/js/slider.js', array( 'jquery'));
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'jquery-ui-core' );
+	wp_enqueue_script( 'uiHelper', get_template_directory_uri() . '/js/uihelper.js', array( 'jquery' ));
+	wp_enqueue_script( 'lillybg', get_template_directory_uri() . '/js/background.js', array( 'jquery', 'uiHelper' ));
 }
 add_action( 'wp_enqueue_scripts', 'lillyscript' );
 
--- wp-content/themes/lillywolf/js/background.js
+++ wp-content/themes/lillywolf/js/background.js
@@ -1,16 +1,11 @@
 
-console.log("help meeeeeee");
-alert("argh help");
+( function( $ ) {	
+		
+	// console.log("found me!");
+	
+	var uiHelper = new UIHelper();
+	uiHelper.watchClipRect("content");
+	uiHelper.drawSkinnyDiagonals("rightPatterns");
+	uiHelper.drawFatDiagonals("leftPatterns");
 
-// ( function( $ ) {	
-// 	// $("#content").text("HI JQUERY");
-// 	
-// 	console.log("found me!");
-// 	
-// 	// var uiHelper = new UIHelper();
-// 	// uiHelper.watchClipRect("content");
-// 	// uiHelper.drawSkinnyDiagonals("rightPatterns");
-// 	// uiHelper.drawFatDiagonals("leftPatterns");
-// 
-// 
-// } )( jQuery );
+} )( jQuery );
