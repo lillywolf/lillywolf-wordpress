866cacb55ccfc3e555e8ec2e16520a04a9f17ad8
lillywolf <lillywolf@gmail.com>
2014-10-21 00:38:43 -0400

    added theme

---
+++ wp-content/themes/lillywolf/footer.php
@@ -0,0 +1,13 @@
+<div id="footer">
+
+<ul class="menu">
+	<li><a href="#/view1">SHOWS</a></li>
+	<li><a href="#/view2">PHOTOS</a></li>
+	<li><a href="#/view2">PRESS</a></li>
+	<li><a href="#/view2">CONTACT</a></li>
+</ul>
+
+</div>
+</div>
+</body>
+</html>
---
+++ wp-content/themes/lillywolf/header.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Lilly Wolf</title>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	<script src="/resources/js/uihelper.js"></script>
+</head>
+<body>
+<div id="wrapper">
+<div id="header">
+	<div id="logo">LILLYWOLF</div>
+	<div id="subtext">WEIRD POP</div>
+</div>
+
+<script type="text/javascript">
+	var uiHelper = new UIHelper();
+	uiHelper.watchClipRect(element[0].id);
+	uiHelper.drawSkinnyDiagonals(element[0].id);
+	uiHelper.drawFatDiagonals(element[0].id);
+</script>
